74e13aa6e6f94ea4d30568e36cecabab346c00c6
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-07-05 19:40:25 +0300

    task 1 js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Profile from "./components/Profile";
+import user from "./tasks/social-profile/user.json";
+
+import Statistics from "./components/Statistics";
+import statisticalData from "./tasks/statistics/statistical-data.json";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Task 1. Social Profile</h2>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <h2>Task 2. Statistics</h2>
+      <Statistics title="Upload stats" stats={statisticalData} />;
     </div>
   );
-}
+};
 
 export default App;
